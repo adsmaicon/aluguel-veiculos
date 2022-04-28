a823a9453f33685240bc993ca2031047ebefba82
Maicon Francisco de Carvalho <Maicon>
2022-04-28 18:15:17 -0300

    feature - adicionado menu

--- main.py
+++ main.py
@@ -1,24 +1,80 @@
+import os
 
 
-menu_principal = """
-# MENU PRICIPAL
-1. Catastro de clintes
-2. Cadastro de veículos
-3. Alugueis
-4. Devoluções
-5. Sair
-"""
 
-menu_cliente = """
-# MENU PRICIPAL
-1. Catastro de clintes
-2. Cadastro de veículos
-3. Alugueis
-4. Devoluções
-5. Sair
-"""
+def menu(titulo, opcoes):
+    while True:
+        print("=" * len(titulo), titulo, "=" * len(titulo), sep="\n")
+        for i, (opcao, funcao) in enumerate(opcoes, 1):
+            print("[{}] - {}".format(i, opcao))
+        print("[{}] - Retornar/Sair".format(i+1))
+        op = input("Opção: ")
+        if op.isdigit():            
+            if int(op) == i + 1:
+                # Encerra este menu e retorna a função anterior
+                break
+            if int(op) <= len(opcoes):
+                # Chama a função do menu:
+                opcoes[int(op) - 1][1]()
+                continue
+        print("Opção inválida. \n\n")
 
 
-while True:
-    print(menu_principal)
-    opcao = input("Opção: ")
+def principal():
+    opcoes = [
+        ("Cadastro de clientes", clientes),
+        ("Cadastro de veículos", veiculos),
+        ("Alugueis", alugueis),
+        ("Devoluções", develucoes),
+    ]
+    return menu("# MENU PRICIPAL #", opcoes)
+
+
+def clientes():
+    opcoes = [
+        ("Inserir pessoa física", selecionar_opcoes),
+        ("Inserir pessoa juridica", selecionar_opcoes),
+        ("Consultar cliente", selecionar_opcoes),
+        ("Remover cliente", selecionar_opcoes),
+        ("Listar todos", selecionar_opcoes),
+    ]
+    return menu("# MENU DE CLIENTES #", opcoes)
+
+
+def veiculos():
+    opcoes = [
+        ("Inserir automóvel", selecionar_opcoes),
+        ("Inserir caminhão", selecionar_opcoes),
+        ("Consultar veículo", selecionar_opcoes),
+        ("Remover veículo", selecionar_opcoes),
+        ("Listar todos", selecionar_opcoes),
+    ]
+    return menu("# MENU DE VEÍCULOS #", opcoes)
+
+
+def alugueis():
+    opcoes = [
+        ("Registar aluguel", selecionar_opcoes),
+        ("Consultar aluguel", selecionar_opcoes),
+        ("Listar alugueis ativos", selecionar_opcoes),
+        ("Listar todos os alugueis", selecionar_opcoes),
+    ]
+    return menu("# MENU DE ALUGUÉIS #", opcoes)
+
+
+
+def develucoes():
+    opcoes = [
+        ("Registrar devolução", selecionar_opcoes),
+        ("Listar alugueis concluídos", selecionar_opcoes),
+        ("Listar todos os alugueis", selecionar_opcoes),
+    ]
+    return menu("# MENU DE DEVOLUÇÕES #", opcoes)
+
+
+def selecionar_opcoes():
+    print("teste")
+
+
+if __name__ == "__main__":
+    principal()
